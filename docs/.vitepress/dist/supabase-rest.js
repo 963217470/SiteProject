e682e3e0918ae45edc4600ccaa038b2efe2d69f1
3158552775 <3158552775@qq.com>
2026-05-23 20:22:44 +0800

    Bust cached Supabase helper for uploads

--- docs/.vitepress/dist/supabase-rest.js
+++ docs/.vitepress/dist/supabase-rest.js
@@ -150,7 +150,7 @@ window.__supabase = {
         upload: async function(path, file) {
           var sess = await ensureSession()
           var token = sess && sess.access_token ? sess.access_token : KEY
-          var headers = { 'apikey': KEY, 'authorization': 'Bearer ' + token }
+          var headers = { 'apikey': KEY, 'authorization': 'Bearer ' + token, 'Authorization': 'Bearer ' + token }
           if (file && file.type) headers['Content-Type'] = file.type
           var res = await fetch(URL + '/storage/v1/object/' + bucket + '/' + encodeURI(path), {
             method: 'POST',
